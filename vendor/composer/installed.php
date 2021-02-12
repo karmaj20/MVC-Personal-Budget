2950412bd89d7fc813e20f866256a61fddf17e2c
Karol Majchrzak <kmajchrzak1999@gmail.com>
2021-02-12 12:47:04 +0100

    added route class

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,24 +1,24 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => '7445be6ebe6eb36e413b85fd103810e7ce1b87f9',
     'name' => '__root__',
   ),
   'versions' => 
   array (
     '__root__' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => '7445be6ebe6eb36e413b85fd103810e7ce1b87f9',
     ),
     'symfony/polyfill-ctype' => 
     array (
