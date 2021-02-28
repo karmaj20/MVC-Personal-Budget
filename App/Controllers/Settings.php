8d4227888b329fcc1e6ebfc7cb9dab3eae358cec
Karol Majchrzak <kmajchrzak1999@gmail.com>
2021-02-28 14:15:43 +0100

    Added twig to settings

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -2,12 +2,18 @@
 
 namespace App\Controllers;
 
+use App\Models\ExpenseMod;
+use App\Models\IncomeMod;
 use Core\View;
 
 class Settings extends Authenticated
 {
     public function newAction()
     {
-        View::renderTemplate('Settings/settings.html');
+        View::renderTemplate('Settings/settings.html',[
+            'incomesCategory'  => IncomeMod::selectIncomesCategory(),
+            'expensesCategory' => ExpenseMod::selectExpensesCategory(),
+            'paymentMethods'   => ExpenseMod::selectPaymentMethodsAssigned()
+        ]);
     }
 }
